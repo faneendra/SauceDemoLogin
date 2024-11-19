dd35127e4f045812b6ac02246d3ad50f99b21059
Vaddaypalli Faneendra <vaddaypalli_faneendra@epam.com>
2024-11-19 12:55:52 +0530

    first commit

---
+++ PageObject/Login.cs
@@ -0,0 +1,12 @@
+﻿namespace PageObject
+{
+    public class Login
+    {
+        protected By UserName => By.Name("user-name");
+        protected By Password => By.XPath("//input[@name='password' and @id = 'password']");
+        protected By SubmitButton => By.Id("login-button");
+        protected By ErrorButton => By.CssSelector(".error-button");
+
+        protected By AddToCartSelect => By.Id("add-to-cart-sauce-labs-backpack");
+    }
+}
---
+++ Utilities/WebUtilities.cs
@@ -0,0 +1,43 @@
+﻿namespace Utilities
+{
+    public class WebUtilities
+    {
+        public readonly IWebDriver _driver;
+        private WaitHelper _waitHelper;
+
+
+       
+
+        public WebUtilities(IWebDriver driver)
+        {
+            this._driver = driver;
+
+            _waitHelper = new WaitHelper(driver);
+
+        }
+        public void Navigate(string url)
+        {
+            _driver.Manage().Window.Maximize();
+            _waitHelper.ImplicitWait();
+            _driver.Navigate().GoToUrl(url);
+
+        }
+        public IWebElement GetWebElement(By locator)
+        {
+            _waitHelper.Explicit(locator, 10);
+            return _driver.FindElement(locator);
+        }
+        public void ClearAndSendKeys(By locator, string value)
+        {
+
+            GetWebElement(locator).Clear();
+            GetWebElement(locator).SendKeys(value);
+
+        }
+        public void IsClick(By locator)
+        {
+            _driver.FindElement(locator).Click();
+        }
+
+    }
+}
