d1337b226d13cd012a5951c5664ffa215b99cf40
faneendra <84117972+faneendra@users.noreply.github.com>
2024-11-19 14:09:47 +0530

    Update PageObject/Login.cs

--- PageObject/Login.cs
+++ PageObject/Login.cs
@@ -8,5 +8,45 @@
         protected By ErrorButton => By.CssSelector(".error-button");
 
         protected By AddToCartSelect => By.Id("add-to-cart-sauce-labs-backpack");
+    public void NavigateToLoginPage()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+        }
+
+        public void EnterCredentials(string username, string password)
+        {
+            driver.FindElement(UserName).SendKeys(username);
+            driver.FindElement(Password).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            driver.FindElement(SubmitButton).Click();
+        }
+
+        public bool IsOnProductsPage()
+        {
+            return driver.Url.Contains("/inventory.html");
+        }
     }
-}
+public void NavigateToLoginPage()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com");
+        }
+
+        public void EnterCredentials(string username, string password)
+        {
+            driver.FindElement(UserName).SendKeys(username);
+            driver.FindElement(Password).SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            driver.FindElement(SubmitButton).Click();
+        }
+
+        public bool IsOnProductsPage()
+        {
+            return driver.Url.Contains("/inventory.html");
+        }
+    }
